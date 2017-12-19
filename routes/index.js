efc716bc34a9c2ecd1facf71e559b0e3338b102e
Akshey Jawa <akshey@crypsis.net>
2017-12-19 18:33:17 +0530

    fixes to make the app work in prod

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 var returnJSON = require('../models/search_model').results;
 
 router.get('/search', returnJSON);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 module.exports = router;
