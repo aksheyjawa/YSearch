4cbec4c9ca9b7efa2b15eb82b48a9411b3d693e7
Akshey Jawa <akshey@crypsis.net>
2017-12-19 19:32:30 +0530

    another fix

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/search', returnJSON);
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 
-app.get('*', (req, res) => {
+router.get('*', (req, res) => {
   res.sendFile(path.join('../client/build/index.html'));
 });
 
