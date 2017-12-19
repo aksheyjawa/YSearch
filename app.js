8749a82da6c223601b9184e4eb68270a034967fb
Akshey Jawa <akshey@crypsis.net>
2017-12-19 19:15:02 +0530

    another fix

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ var app = express();
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -32,6 +28,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
-//var path = require('path');
 
 var returnJSON = require('../models/search_model').results;
 
 router.get('/search', returnJSON);
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-
 
 module.exports = router;
