77133bc12303d735926fad48630b0be77f2ed7e9
Akshey Jawa <aksheyjawa@gmail.com>
2018-07-28 14:17:42 +0530

    Database login

--- config/db.js
+++ config/db.js
@@ -17,7 +17,7 @@ let mongoURL = "", db_name = "";
 mongoURL = 'mongodb://localhost:27017/indexes';
 db_name = 'indexes';
 
-//mongoURL = 'mongodb://aksheyjawa:qwerty123#@ds141185.mlab.com:41185/yl-mongo';
-//db_name = 'yl-mongo';
+mongoURL = 'mongodb://aksheyjawa:canbetold123@ds259175.mlab.com:59175/ysearch';
+db_name = 'ysearch';
 
 module.exports = {mongoURL, db_name};
